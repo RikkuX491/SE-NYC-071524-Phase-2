225dab40ffe409dad0f304a11385781a8743081f
Ricardo Guerra <RikkuG491@gmail.com>
2024-08-07 16:34:43 -0400

    Removed example code from PetPage.js

--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,21 +1,9 @@
 import PetList from "./PetList";
 import pets from "../data/pets";
 
-import {useState} from "react";
-
 console.log(pets)
 
 function PetPage(){
-
-    const [searchText, setSearchText] = useState("")
-
-    function handleChange(event){
-        setSearchText(event.target.value)
-    }
-
-    const filteredPets = pets.filter(pet => {
-        return pet.name.toUpperCase().includes(searchText.toUpperCase())
-    })
 
     return (
         <main>
@@ -25,10 +13,9 @@ function PetPage(){
                     type="text"
                     id="search"
                     placeholder="Type a name to search..."
-                    onChange={handleChange}
                 />
             </div>
-            <PetList pets={filteredPets}/>
+            <PetList pets={pets}/>
         </main>
     );
 }
