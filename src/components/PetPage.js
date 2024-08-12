48ea1e59a9a480d53c64079f5e48422310664d97
Ricardo Guerra <RikkuG491@gmail.com>
2024-08-12 12:51:38 -0400

    Added example code from the lecture

--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,5 +1,5 @@
 import PetList from "./PetList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from './Search';
 import NewPetForm from "./NewPetForm";
 
@@ -7,6 +7,42 @@ function PetPage(){
 
     const [pets, setPets] = useState([])
     const [searchText, setSearchText] = useState("")
+
+    // console.log("Here's some code written before the useEffect code...")
+
+    useEffect(() => {
+
+        // console.log('useEffect code!')
+
+        // setPets([
+        //     {
+        //         id: 1,
+        //         name: "Fido",
+        //         image: "./images/dog.jpeg",
+        //         animal_type: "Dog"
+        //     }
+        // ])
+
+        fetch("http://localhost:4000/pets")
+        .then(response => response.json())
+        .then(petsData => setPets(petsData))
+    }, [])
+
+    // Using useEffect to handle a different side effect that occurs upon initial component render and whenever the pets state updates
+    useEffect(() => {
+        console.log("You've updated the pets state")
+    }, [pets])
+
+    // console.log("Here's some code written after the useEffect code...")
+
+    // setPets([
+    //     {
+    //         id: 1,
+    //         name: "Fido",
+    //         image: "./images/dog.jpeg",
+    //         animal_type: "Dog"
+    //     }
+    // ])
 
     const filteredPets = pets.filter(pet => {
         return pet.name.toUpperCase().includes(searchText.toUpperCase())
@@ -23,7 +59,30 @@ function PetPage(){
     }
 
     function addPet(newPet){
-        setPets([...pets, newPet])
+        // Optimistic rendering approach with POST request
+        // setPets([...pets, newPet])
+        
+        // console.log(newPet)
+
+        // fetch('http://localhost:4000/pets', {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify(newPet)
+        // })
+
+        // Pessimistic rendering approach with POST request
+        fetch('http://localhost:4000/pets', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(newPet)
+        })
+        .then(response => response.json())
+        .then(newPet => setPets([...pets, newPet]))
     }
 
     return (
